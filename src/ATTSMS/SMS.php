bf5ee7783e84546499974045bd720bab72413766
Arnold Asllani <asllani94@gmail.com>
2017-10-05 17:18:36 +0300

    Updated sms.php
    renamed unmatching variable

--- src/ATTSMS/SMS.php
+++ src/ATTSMS/SMS.php
@@ -96,7 +96,7 @@ class SMS
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $address);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,$send_xml);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$xml);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
